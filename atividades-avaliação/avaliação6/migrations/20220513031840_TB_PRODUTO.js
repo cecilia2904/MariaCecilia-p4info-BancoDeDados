98da7598d4096eb2b2e0d64abcb9de92afdb32fc
Janl_ <maria.cecilia.freitas07@aluno.ifce.edu.br>
2022-06-28 22:45:18 -0300

    .

---
+++ atividades-avaliação/avaliação6/migrations/20220513031840_TB_PRODUTO.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+ exports.up = function(knex, Promise) {
+    return knex.schema.createTable('TB_PRODUTO', table => {
+        table.increments('id').primary()
+        table.integer('codigo').unique().notNull()
+        table.string('descricao').notNull()
+        table.double('valor')
+            .notNull().defaultTo(true)
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('TB_PRODUTO')
+};
